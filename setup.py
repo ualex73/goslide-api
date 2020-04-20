6d675b3ba979aa175b7f26927c9507b7565fb4b6
ualex73 <ualex73@gmail.com>
2020-04-20 21:39:58 +0200

    Bumped to 0.6.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='goslide-api',
-    version='0.6.1',
+    version='0.6.2',
     url='https://github.com/ualex73/goslide-api',
     license='Apache License 2.0',
     author='Alexander Kuiper',
