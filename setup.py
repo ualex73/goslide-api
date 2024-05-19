a2096e4af40108cf581abfb55ba57ce5ca8bd3d4
Alexander <ualex73@users.noreply.github.com>
2024-05-19 11:24:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.pypi') as f:
 
 setuptools.setup(
     name='goslide-api',
-    version='0.6.5',
+    version='0.6.7',
     url='https://github.com/ualex73/goslide-api',
     license='Apache License 2.0',
     author='Alexander Kuiper',
